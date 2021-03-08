5219be5dbd3bcd6d6c496119da1956b9c87fdc99
ricky-tham <ricky.tham@ontariotechu.net>
2021-03-07 22:30:45 -0500

    Changed hashmaps to tree maps and edited javadoc to match

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,5 @@
 package sample;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
@@ -6,6 +7,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.DirectoryChooser;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -13,14 +15,22 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 public class Controller {
-    @FXML private TableView<TestFile> table;
-    @FXML private TableColumn<TestFile, String> file;
-    @FXML private TableColumn<TestFile, String> realClass;
-    @FXML private TableColumn<TestFile, Double> probability;
-    @FXML private TextField accuracyVal;
-    @FXML private TextField precisionVal;
-    @FXML private TextField trainDirectory;
-    @FXML private TextField testDirectory;
+    @FXML
+    private TableView<TestFile> table;
+    @FXML
+    private TableColumn<TestFile, String> file;
+    @FXML
+    private TableColumn<TestFile, String> realClass;
+    @FXML
+    private TableColumn<TestFile, Double> probability;
+    @FXML
+    private TextField accuracyVal;
+    @FXML
+    private TextField precisionVal;
+    @FXML
+    private TextField trainDirectory;
+    @FXML
+    private TextField testDirectory;
     double truePositives = 0;
     double trueNegative = 0;
     double falsePositives = 0;
@@ -28,22 +38,21 @@ public class Controller {
     double accuracy = 0.0;
     double precision = 0.0;
 
-    // creating hashmaps for use in functions
-    private HashMap<String,Double> hamFreq = new HashMap<String, Double>();
-    private HashMap<String,Double> spamFreq = new HashMap<String, Double>();
-    private HashMap<String,Integer> hamWordCount = new HashMap<String,Integer>();
-    private HashMap<String,Integer> spamWordCount = new HashMap<String,Integer>();
-    private HashMap<String,Double> spamWord = new HashMap<String, Double>();
+    // creating treemaps for use in functions
+    private TreeMap<String, Double> hamFreq = new TreeMap<String, Double>();
+    private TreeMap<String, Double> spamFreq = new TreeMap<String, Double>();
+    private TreeMap<String, Integer> hamWordCount = new TreeMap<String, Integer>();
+    private TreeMap<String, Integer> spamWordCount = new TreeMap<String, Integer>();
+    private TreeMap<String, Double> spamWord = new TreeMap<String, Double>();
 
     /*
-    * Checks if a word is valid based on a condition
-    * @param word   a word in one of the files
-    * @return       true or false based on condition
-    */
-    private boolean isValidWord(String word){
+     * Checks if a word is valid based on a condition
+     * @param word   a word in one of the files
+     * @return       true or false based on condition
+     */
+    private boolean isValidWord(String word) {
         String condition = "^[a-zA-Z]*$";
-        if (word.matches(condition))
-        {
+        if (word.matches(condition)) {
             return true;
         } else {
             return false;
@@ -52,15 +61,15 @@ public class Controller {
 
     /*
      * Calculates the probability that the word W appears in a ham file (Pr(W|H))
-     * Uses a hashmap to store words and their counts
+     * Uses a treemap to store words and their counts
      * @param file   a file within the selected directory
      */
-    public void prWH (File file) throws IOException {
+    public void prWH(File file) throws IOException {
         File[] files = file.listFiles();
         System.out.println("Number of files ham: " + files.length);
 
         for (int i = 0; i < files.length; i++) {
-            HashMap<String, Integer> temp = new HashMap<String, Integer>();
+            TreeMap<String, Integer> temp = new TreeMap<String, Integer>();
 
             // put list of words in specific file to temporary map
             Scanner scanner = new Scanner(files[i]);
@@ -71,7 +80,7 @@ public class Controller {
                 }
             }
 
-            // iterate through temp hashmap and insert word list to a wordCount map
+            // iterate through temp treemap and insert word list to a wordCount map
             for (Map.Entry<String, Integer> entry : temp.entrySet()) {
                 // checks if hamWordCount contains the key
                 if (hamWordCount.containsKey(entry.getKey())) {
@@ -88,8 +97,8 @@ public class Controller {
 
             // calculates the frequency of word in ham file
             // puts it into a map
-            for (Map.Entry<String,Integer> entry: hamWordCount.entrySet()){
-                double resultHam = (double)entry.getValue() / (double)files.length;
+            for (Map.Entry<String, Integer> entry : hamWordCount.entrySet()) {
+                double resultHam = (double) entry.getValue() / (double) files.length;
                 hamFreq.put(entry.getKey(), resultHam);
             }
         }
@@ -97,15 +106,16 @@ public class Controller {
 
     /*
      * Calculates the probability that the word W appears in a spam file (Pr(W|S))
-     * Uses a hashmap to store words and their counts
+     * Uses a treemap to store words and their counts
      * @param file   a file within the selected directory
      */
-    public void prWS (File file) throws IOException {
+    public void prWS(File file) throws IOException {
         File[] files = file.listFiles();
+        assert files != null;
         System.out.println("Number of files spam: " + files.length);
 
         for (int i = 0; i < files.length; i++) {
-            HashMap<String, Integer> temp = new HashMap<String, Integer>();
+            TreeMap<String, Integer> temp = new TreeMap<String, Integer>();
 
             // put list of words in specific file to temporary map
             Scanner scanner = new Scanner(files[i]);
@@ -116,7 +126,7 @@ public class Controller {
                 }
             }
 
-            // iterate through temp hashmap and insert word list to a wordCount map
+            // iterate through temp treemap and insert word list to a wordCount map
             for (Map.Entry<String, Integer> entry : temp.entrySet()) {
                 // checks if hamWordCount contains the key
                 if (spamWordCount.containsKey(entry.getKey())) {
@@ -133,8 +143,8 @@ public class Controller {
 
             // calculates the frequency of word in spam file
             // puts it into a map
-            for (Map.Entry<String,Integer> entry: spamWordCount.entrySet()){
-                double resultSpam = (double)entry.getValue() / (double)files.length;
+            for (Map.Entry<String, Integer> entry : spamWordCount.entrySet()) {
+                double resultSpam = (double) entry.getValue() / (double) files.length;
                 spamFreq.put(entry.getKey(), resultSpam);
             }
         }
@@ -142,11 +152,11 @@ public class Controller {
 
     /*
      * Calculates the probability a word is spam (Pr(S|W))
-     * Uses a hashmap to store words and their counts
+     * Uses a treemap to store words and their counts
      */
-    public void prSW(){
-        for(Map.Entry<String, Double> entry: spamFreq.entrySet()){
-            if(hamFreq.containsKey(entry.getKey())){
+    public void prSW() {
+        for (Map.Entry<String, Double> entry : spamFreq.entrySet()) {
+            if (hamFreq.containsKey(entry.getKey())) {
                 double resultFinal = entry.getValue() / (entry.getValue() + hamFreq.get(entry.getKey()));
                 spamWord.put(entry.getKey(), resultFinal);
             }
@@ -159,7 +169,7 @@ public class Controller {
      * @param file   a file within the selected directory
      * @return       the final probability of a file being spam
      */
-    public double prSF(File file) throws FileNotFoundException{
+    public double prSF(File file) throws FileNotFoundException {
         double result = 0.0;
         double threshold = 0.5;
         double probSF;
@@ -167,7 +177,7 @@ public class Controller {
         while (scanner.hasNext()) {
             String word = scanner.next();
             if (isValidWord(word) && spamWord.containsKey(word)) {
-                result += Math.log( (1-spamWord.get(word) - Math.log(spamWord.get(word))));
+                result += Math.log((1 - spamWord.get(word) - Math.log(spamWord.get(word))));
             }
         }
         //System.out.println(result);
@@ -177,13 +187,13 @@ public class Controller {
         //System.out.println(probSF);
 
         // Accuracy and Precision
-        if (file.getParent().contains("spam") && probSF > threshold){
+        if (file.getParent().contains("spam") && probSF > threshold) {
             truePositives += 1;
         }
-        if (file.getParent().contains("ham") && probSF > threshold){
+        if (file.getParent().contains("ham") && probSF > threshold) {
             falsePositives += 1;
         }
-        if (file.getParent().contains("ham") && probSF < threshold){
+        if (file.getParent().contains("ham") && probSF < threshold) {
             trueNegative += 1;
         }
         numTestingFiles += 1;
@@ -194,38 +204,32 @@ public class Controller {
      * Check what type of directory and calls the appropriate method to process the files
      * @param file   a file within the selected directory
      */
-    public void runProcessTraining(File file){
-        if(file.isDirectory()){
-            if(file.getName().equals("ham")){
-                try{
+    public void runProcessTraining(File file) {
+        if (file.isDirectory()) {
+            if (file.getName().equals("ham")) {
+                try {
                     prWH(file);
-                }
-                catch(IOException error){
+                } catch (IOException error) {
                     error.printStackTrace();
                 }
                 System.out.println("Finished Ham Folder");
-            }
-            else if(file.getName().equals("ham2")){
-                try{
+            } else if (file.getName().equals("ham2")) {
+                try {
                     prWH(file);
-                }
-                catch(IOException error){
+                } catch (IOException error) {
                     error.printStackTrace();
                 }
                 System.out.println("Finished Ham2 Folder");
-            }
-            else if(file.getName().equals("spam")){
-                try{
+            } else if (file.getName().equals("spam")) {
+                try {
                     prWS(file);
-                }
-                catch(IOException error){
+                } catch (IOException error) {
                     error.printStackTrace();
                 }
                 System.out.println("Finished Spam Folder");
-            }
-            else{
+            } else {
                 File[] files = file.listFiles();
-                for(int i = 0; i < files.length; i++){
+                for (int i = 0; i < files.length; i++) {
                     runProcessTraining(files[i]);
                 }
             }
@@ -237,17 +241,16 @@ public class Controller {
      * Calls corresponding method for calculation of spam probability
      * @param event   the action of pressing a button on the screen
      */
-    public void trainAction(ActionEvent event){
+    public void trainAction(ActionEvent event) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setInitialDirectory(new File("."));
         File a = directoryChooser.showDialog(null);
-        if(a != null){
+        if (a != null) {
             String path = a.getAbsolutePath();
             trainDirectory.setText(path);
             runProcessTraining(a);
             prSW();
-        }
-        else{
+        } else {
             System.out.println("Invalid Directory");
         }
     }
@@ -257,29 +260,26 @@ public class Controller {
      * Adds the probability value to the table
      * @param file   a file within the selected directory or a directory itself
      */
-    public void runProcessTesting(File file){
-        if (file.isDirectory()){
+    public void runProcessTesting(File file) {
+        if (file.isDirectory()) {
             // goes through recursively if a directory of files
             File[] files = file.listFiles();
-            for(int i = 0; i < files.length; i++){
+            for (int i = 0; i < files.length; i++) {
                 runProcessTesting(files[i]);
             }
-        }
-        else if(file.exists()){
+        } else if (file.exists()) {
             double spamProbability = 0.0;
-            try{
+            try {
                 spamProbability = prSF(file);
-            }
-            catch(IOException error){
+            } catch (IOException error) {
                 error.printStackTrace();
             }
             // take probability of the test files and add to tableView
             //had to change decimal formatting to a string instead of double to make .format work
             DecimalFormat decimalFormat = new DecimalFormat("0.00000");
-            if(file.getParent().contains("ham")){
+            if (file.getParent().contains("ham")) {
                 table.getItems().add(new TestFile(file.getName(), decimalFormat.format(spamProbability), "ham"));
-            }
-            else{
+            } else {
                 table.getItems().add(new TestFile(file.getName(), decimalFormat.format(spamProbability), "spam"));
             }
         }
@@ -292,11 +292,11 @@ public class Controller {
      * Fills each row of the chart with the proper data
      * @param event   the action of pressing a button on the screen
      */
-    public void testAction(ActionEvent event){
+    public void testAction(ActionEvent event) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setInitialDirectory(new File("."));
         File a = directoryChooser.showDialog(null);
-        if(a != null){
+        if (a != null) {
             String path = a.getAbsolutePath();
             testDirectory.setText(path);
             runProcessTesting(a);
@@ -310,12 +310,11 @@ public class Controller {
             precision = truePositives / (falsePositives + trueNegative);
             accuracyVal.setText(decimalFormat.format(accuracy));
             precisionVal.setText(decimalFormat.format(precision));
-            // add all the values to the tableView
+            // add all the values to the table
             file.setCellValueFactory(new PropertyValueFactory<TestFile, String>("filename"));
             realClass.setCellValueFactory(new PropertyValueFactory<TestFile, String>("actualClass"));
             probability.setCellValueFactory(new PropertyValueFactory<TestFile, Double>("spamProbability"));
-        }
-        else{
+        } else {
             System.out.println("Invalid Directory");
         }
     }
